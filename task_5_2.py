14a180f8942cd109cea60892a8857d7a04aafa5b
ChristophKarthe <christoph.karthe@web.de>
2024-12-27 12:58:58 +0100

    5_2 finished with deprecated code in comments

--- task_5_2.py
+++ task_5_2.py
@@ -2,7 +2,7 @@
 import math
 import operator
 # Arrays befüllen
-textfile = open('./input_5_example.txt','r')
+textfile = open('./input_5.txt','r')
 array1 = textfile.read().split("\n")
 array2 = []
 k = -1
@@ -22,24 +22,22 @@ for elements in array2:
     array2new.append(elements.split(","))
 array1 = array1new
 array2 = array2new
-array1.reverse()
 array2.reverse()
-
-dic = {}
-for elements in array1:
-    dic[elements[0]] = 0
-for elements in array1:
-    dic[elements[0]] += 1
-copyarray1 = []
-for i in range(0, len(dic)):
-    maxkey = max(dic.items(), key=operator.itemgetter(1))[0]
-    for i in range(0, max(dic.items(), key=operator.itemgetter(1))[1]):
-        for elements in array1:
-            if elements[0] == maxkey:
-                copyarray1.append(elements)
-                array1.pop(array1.index(elements))
-    dic.pop(max(dic.items(), key=operator.itemgetter(1))[0])
-array1 = copyarray1
+# dic = {}
+# for elements in array1:
+#     dic[elements[0]] = 0
+# for elements in array1:
+#     dic[elements[0]] += 1
+# copyarray1 = []
+# for i in range(0, len(dic)):
+#     maxkey = min(dic.items(), key=operator.itemgetter(1))[0]
+#     for i in range(0, max(dic.items(), key=operator.itemgetter(1))[1]):
+#         for elements in array1:
+#             if elements[0] == maxkey:
+#                 copyarray1.append(elements)
+#                 array1.pop(array1.index(elements))
+#     dic.pop(min(dic.items(), key=operator.itemgetter(1))[0])
+# array1 = copyarray1
 def checkarray(elements, array1):
     superIsSafe: bool = True
     for element in elements:
@@ -58,25 +56,58 @@ def checkarray(elements, array1):
             superIsSafe = isSafe
             break
     return superIsSafe
-# mit den Arrays Arbeiten
+# # mit den Arrays Arbeiten
 addingarray = []
 sum = 0
+add = False
+change = False
 for elements in array2:
-    if not checkarray(elements, array1):
-        dic = {}
+    c = 0
+    while not checkarray(elements, array1):
+        #print("while")
+        if c > 10000:
+            break
         for element in elements:
-            dic[element] = 0
-        for items in array1:
-            if array1[array1.index(items)][0] in dic.keys() and array1[array1.index(items)][1] in dic.keys():
-                add = dic[array1[array1.index(items)][1]]
-                dic[array1[array1.index(items)][0]] += add + 1
-        copyelements = []
-        for element in elements:
-            copyelements.append(max(dic.items(), key=operator.itemgetter(1))[0])
-            dic.pop(max(dic.items(), key=operator.itemgetter(1))[0])
-        halfindex = math.ceil(len(copyelements)/2)-1
-        addingarray.append(copyelements[halfindex])
+            for items in array1:
+                if len(elements) == (elements.index(element)+1):
+                    #print("next")
+                    break
+                # for i in range(0, elements.index(element)+1):
+                #     if items[1] == elements[i] and items[0] == elements[elements.index(element)+1]:
+                #         print("hit1")
+                #         change = True
+                for i in range(elements.index(element)+1, len(elements)):
+                    if items[0] == elements[i] and items[1] == element:
+                        change = True
+                if change:
+                        h = elements[elements.index(element)+1]
+                        elements[elements.index(element)+1]= elements[elements.index(element)]
+                        elements[elements.index(element)] = h
+                        change = False
+                        break
+        add = True
+        c += 1
+    if add:
+        halfindex = math.ceil(len(elements)/2)-1
+        addingarray.append(array2[array2.index(elements)][halfindex])
+    add = False
 
+# for elements in array2:
+#     if not checkarray(elements, array1):
+#         dic = {}
+#         for element in elements:
+#             dic[element] = 0
+#         for items in array1:
+#             if array1[array1.index(items)][0] in dic.keys() and array1[array1.index(items)][1] in dic.keys():
+#                 add = dic[array1[array1.index(items)][1]]
+#                 dic[array1[array1.index(items)][0]] += add + 1
+#         copyelements = []
+#         for element in elements:
+#             copyelements.append(max(dic.items(), key=operator.itemgetter(1))[0])
+#             dic.pop(max(dic.items(), key=operator.itemgetter(1))[0])
+#         halfindex = math.ceil(len(copyelements)/2)-1
+#         addingarray.append(copyelements[halfindex])
+    
 for elements in addingarray:
     sum += int(elements)
 print(sum)
