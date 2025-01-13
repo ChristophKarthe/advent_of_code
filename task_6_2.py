9e07004d01b675b4a91e4d99fd31e39c0c0ac71b
ChristophKarthe <christoph.karthe@web.de>
2025-01-13 10:49:34 +0100

    ellaborated on Cocept for task 6_2

--- task_6_2.py
+++ task_6_2.py
@@ -1,12 +1,14 @@
 # imports
 
 # alle positionen durchgehen und für jeden Punkt
-# das Objekt hinsetzen und herausfinden ob ein Loop stattfindet
-# bloß wie erkennt man einen Loop?
-# - nach einer bestimmten Zeit abbrechen --> schlechte Lösung
-# - Turns übeprüfen
-# - Pfad überprüfen (wenn er einen Pfad geht, der schon meherere X'e hintereinander hat, ist es ein Loop?)
+    # das Objekt hinsetzen und herausfinden ob ein Loop stattfindet
+        # bloß wie erkennt man einen Loop?
+        # - nach einer bestimmten Zeit abbrechen --> schlechte Lösung
+        # - Turns übeprüfen --> keine Lösung
+        # - Pfad überprüfen (wenn er einen Pfad geht, der schon meherere X'e hintereinander hat, ist es ein Loop?)
+        # - 2. + 3. gemeinsam die Lösung?
+        # -> X vor Turn, Turn , X nach Turn !!!
 
 
-# wenn Loop stattfindet, speichern
+    # wenn Loop stattfindet, speichern
 # am Ende die Anzahl der Loops ausgeben
